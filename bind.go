ec2ac71d26ab9a8b187ad98d63735593c098dd7b
Liam Stanley <me@liamstanley.io>
2022-05-31 01:23:02 -0400

    feat(bind): add validation support using go-playground
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- bind.go
+++ bind.go
@@ -11,9 +11,19 @@ import (
 	"strings"
 
 	"github.com/go-playground/form/v4"
+	"github.com/go-playground/validator/v10"
 )
 
-var DefaultDecoder = form.NewDecoder()
+var (
+	// DefaultDecoder is the default decoder used by Bind. You can either override
+	// this, or provide your own. Make sure it is set before Bind is called.
+	DefaultDecoder = form.NewDecoder()
+
+	// DefaultValidator is the default validator used by Bind, when the provided
+	// struct to the Bind() call doesn't implement Validatable. Set this to nil
+	// to disable validation using go-playground/validator.
+	DefaultValidator = validator.New()
+)
 
 // Validatable is an interface that can be implemented by structs to
 // provide custom validation logic, on top of the default go-playground/form
@@ -25,14 +35,16 @@ type Validatable interface {
 // Bind decodes the request body to the given struct. Take a look at
 // DefaultDecoder to add additional customizations to the default decoder.
 // You can add additional customizations by using the Validatable interface,
-// with a custom implementation of the Validate() method on v.
+// with a custom implementation of the Validate() method on v. Alternatively,
+// chix also supports the go-playground/validator package, which allows various
+// validation methods via struct tags.
 //
 // At this time the only supported content-types are application/json,
 // application/x-www-form-urlencoded, as well as GET parameters.
 //
 // If validation fails, Bind will respond with an HTTP 400 Bad Request, using
 // Error().
-func Bind(w http.ResponseWriter, r *http.Request, v interface{}) (ok bool) {
+func Bind(w http.ResponseWriter, r *http.Request, v any) (ok bool) {
 	var err, rerr error
 
 	if err = r.ParseForm(); err != nil {
@@ -70,6 +82,23 @@ handle:
 			_ = Error(w, r, http.StatusBadRequest, err)
 			return false
 		}
+
+		return true
 	}
+
+	if DefaultValidator != nil {
+		if err := DefaultValidator.Struct(v); err != nil {
+			if _, ok := err.(*validator.InvalidValidationError); ok {
+				panic(fmt.Errorf("invalid validation error: %w", err))
+			}
+
+			// for _, err := range err.(validator.ValidationErrors) {}
+			_ = Error(w, r, http.StatusBadRequest, err)
+			return false
+		}
+
+		return true
+	}
+
 	return true
 }
--- go.mod
+++ go.mod
@@ -6,19 +6,26 @@ require (
 	github.com/apex/log v1.9.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-playground/form/v4 v4.2.0
+	github.com/go-playground/validator/v10 v10.11.0
 	github.com/gorilla/sessions v1.2.1
-	github.com/lrstanley/go-bogon v0.0.0-20220429030658-4fc261762ff8
+	github.com/lrstanley/go-bogon v0.0.0-20220507183221-362a880cf97b
 	github.com/markbates/goth v1.72.0
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29
 )
 
 require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	golang.org/x/oauth2 v0.0.0-20220524215830-622c5d57e401 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
