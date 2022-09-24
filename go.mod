e3e8c7537feb1196366c5862c9b5830d65a851a6
Liam Stanley <me@liamstanley.io>
2022-09-24 13:28:40 -0400

    feat: add prometheus metrics support
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- go.mod
+++ go.mod
@@ -10,22 +10,29 @@ require (
 	github.com/gorilla/sessions v1.2.1
 	github.com/lrstanley/go-bogon v0.0.0-20220507183221-362a880cf97b
 	github.com/markbates/goth v1.73.0
-	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29
+	github.com/prometheus/client_golang v1.13.0
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
 	golang.org/x/oauth2 v0.0.0-20220524215830-622c5d57e401 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
---
+++ prometheus.go
@@ -0,0 +1,62 @@
+// Copyright (c) Liam Stanley <me@liamstanley.io>. All rights reserved. Use
+// of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+package chix
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	metricHTTPDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "http_duration_seconds",
+			Help: "HTTP request latencies in seconds.",
+		},
+		[]string{"method", "path", "status"},
+	)
+	metricHTTPCount = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests made.",
+		},
+		[]string{"method", "path", "status"},
+	)
+	metricHTTPBytes = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_response_bytes",
+			Help: "Total number of bytes sent in response to HTTP requests.",
+		},
+		[]string{"method", "path", "status"},
+	)
+)
+
+func UsePrometheus(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrappedWriter := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+		start := time.Now()
+		next.ServeHTTP(wrappedWriter, r)
+		elapsed := time.Since(start)
+
+		rctx := chi.RouteContext(r.Context())
+
+		labels := prometheus.Labels{
+			"method": r.Method,
+			"path":   rctx.RoutePattern(),
+			"status": strconv.Itoa(wrappedWriter.Status()),
+		}
+
+		metricHTTPDuration.With(labels).Observe(elapsed.Seconds())
+		metricHTTPCount.With(labels).Inc()
+		metricHTTPBytes.With(labels).Add(float64(wrappedWriter.BytesWritten()))
+	})
+}
